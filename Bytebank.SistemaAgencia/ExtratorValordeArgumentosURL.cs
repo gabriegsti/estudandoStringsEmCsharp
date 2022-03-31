ac74b1839b6e28f86d67421ff0bdf5214f21faa0
gabrielgsti <gabrielgsdevbr@gmail.com>
2022-03-31 14:17:01 -0300

    manipulando Strings com substring remove e utros metodos

---
+++ Bytebank.SistemaAgencia/ExtratorValordeArgumentosURL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank.SistemaAgencia
+{
+    public class ExtratorValordeArgumentosURL
+    {
+        private readonly string _argumentos;
+        public string URL { get;}
+        public ExtratorValordeArgumentosURL(string url)
+        {
+            if (String.IsNullOrEmpty(url))
+            {
+                throw new ArgumentNullException("O argumento url não pode ser nulo ou vazio", nameof(url));
+            }
+           
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos= url.Substring(indiceInterrogacao+1);
+
+        }
+
+        //moedaOrigem = real & moedaDestino=dolar
+        public string GetValor(string nomeParametro)
+        {
+            string termo = nomeParametro + "=";
+            int indiceTermo = _argumentos.IndexOf(termo);
+
+            string resultado = _argumentos.Substring(indiceTermo + termo.Length);
+            int indiceECormecial = resultado.IndexOf('&');
+            
+            if(indiceECormecial == -1)
+            {
+                return resultado;
+            }
+
+            return resultado.Remove(indiceECormecial);
+        }
+
+    }
+}
---
+++ Bytebank.SistemaAgencia/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bytebank.SistemaAgencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
+            ExtratorValordeArgumentosURL extratorDeValores = new ExtratorValordeArgumentosURL(urlParametros);
+
+            
+            string valor = extratorDeValores.GetValor("moedaDestino");
+            Console.WriteLine("Valor de moedaDestino: " + valor);
+
+            Console.ReadLine();
+
+
+
+            //Testando o remove
+            string testeRemocao = "primeiraParte&parteParaRemover";
+            int indiceEComercial = testeRemocao.IndexOf('&');
+            Console.WriteLine(testeRemocao.Remove(indiceEComercial, 4));
+
+            Console.ReadLine();
+
+               
+
+
+            // testando substring
+            string palavra = "moedaOrigem=real&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino";
+            int indice = palavra.IndexOf(nomeArgumento);
+            Console.WriteLine(indice);
+
+            Console.WriteLine("Tamanho da string " + palavra.Length);
+
+            Console.WriteLine(palavra.Substring(indice));
+            //Lembre-se Substring é inclusivo, ou seja ele considera a partir da posição que é fornecida.
+            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length + 1));
+            Console.ReadLine();
+
+
+
+            //Testando o IsNullOrEmpty
+            string textoVazio = "";
+            Console.WriteLine(String.IsNullOrEmpty(textoVazio));
+
+            string textoNulo = null;
+            Console.WriteLine(String.IsNullOrEmpty(textoNulo));
+
+            string textoQualquqer = "Ave";
+            Console.WriteLine(String.IsNullOrEmpty(textoQualquqer));
+
+            Console.ReadLine();
+
+            string url = "paginas?moedaOrigem=real&moedaDestino=dolar";
+
+            int indiceInterrogacao = url.IndexOf('?');
+
+
+            ExtratorValordeArgumentosURL extrator = new ExtratorValordeArgumentosURL("");
+
+
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
